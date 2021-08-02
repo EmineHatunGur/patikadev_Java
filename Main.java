93fa7c71b4271970a64f6080e67245aef943a9a0
Emine <74425990+EmineHatunGur@users.noreply.github.com>
2021-08-02 19:12:29 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+       Employee emp=new Employee("ahmet",5850,50,2012);
+
+       emp.toMyString();
+
+    }
+}
